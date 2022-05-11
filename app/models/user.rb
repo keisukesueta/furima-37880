d56e3b7f931dd7fa962c0e98959e295d44b8f6e8
keisukesueta <103096045+keisukesueta@users.noreply.github.com>
2022-05-11 15:59:30 +0900

    エラーメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-         validates :nickname,           presence: true
-         validates :email,              presence: true
-         validates :encrypted_password, presence: true   
-         validates :birth_day,          presence: true  
-         validates :lastname,           presence: true  
-         validates :firstname,          presence: true  
-         validates :lastname_kana,      presence: true  
-         validates :firstname_kana,     presence: true
+      
+         #has_many :items
+         #has_many :orders
+         # has_many :comments 
 
 
+        with_options presence: true do
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
+         validates :nickname,           uniqueness: true
+         validates :email,              uniqueness: true
+         #validates :encrypted_password, presence: true   
+         validates :birth_day,          presence: true 
+         validates :lastname,           format: { with: /\A[ぁ-んァ-ン一-龥]/ }  
+         validates :firstname,          format: { with: /\A[ぁ-んァ-ン一-龥]/ }  
+         validates :lastname_kana,      format: { with: /\A[ァ-ヶー－]+\z/ } 
+         validates :firstname_kana,     format: { with: /\A[ァ-ヶー－]+\z/ }
+        end
 end
