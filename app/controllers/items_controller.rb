320c82aa5a13c3ec489f39016b71dbb9a001714a
keisukesueta <103096045+keisukesueta@users.noreply.github.com>
2022-05-12 16:29:04 +0900

    Active Storageの導入とつい記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,18 +3,31 @@ class ItemsController < ApplicationController
 
   # before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
-    # @items = Item.includes(:user).order('created_at DESC')
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
-  # def new
-  # @item = Item.new
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+   if @item.save
+    redirect_to root_path
+   else
+    render :new
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shippingcost_id, :shippingaddress_id, :shippingday_id, :price).merge(user_id: current_user.id)
+  end
+
+  # def set_item
+    # @item = Item.find(params[:id])
   # end
 
-  # def create
-  # @item = Item.new(item_params)
-  # if @item.save
-  # redirect_to root_path
-  # else
-  # render :new
-  # end
 end
+
