0005caa5c8c053f9ac375eab6b14ed6334050603
keisukesueta <103096045+keisukesueta@users.noreply.github.com>
2022-05-14 22:14:54 +0900

    編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  # before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,13 +19,34 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  # def edit
+    #if @item.user_id == current_user.id && @item.order.nil?
+    #else
+      #redirect_to root_path
+    #end
+  #end
+
+  #def update
+    #@item.update(item_params)
+    #if @item.valid?
+      #redirect_to item_path(item_params)
+    #else
+      #render 'edit'
+    #end
+  #end
+
+
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shippingcost_id, :shippingaddress_id,:shippingday_id, :price).merge(user_id: current_user.id)
   end
 
-  # def set_item
-  # @item = Item.find(params[:id])
-  # end
+   def set_item
+   @item = Item.find(params[:id])
+   end
 end
