ead24fe6a6368f49e242c3ff41f3c1b90fc5b2d8
keisukesueta <103096045+keisukesueta@users.noreply.github.com>
2022-05-16 12:17:45 +0900

    コントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  before_action :set_item, only: [:show] #:edit, :update, :destroy]
+  before_action :set_item, only: [:show :edit, :update] #:destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -22,21 +22,21 @@ class ItemsController < ApplicationController
   def show
   end
 
-  # def edit
-    #if @item.user_id == current_user.id && @item.order.nil?
-    #else
-      #redirect_to root_path
-    #end
-  #end
+  def edit
+    if @item.user_id == current_user.id #&& @item.order.nil?
+    else
+      redirect_to root_path
+    end
+  end
 
-  #def update
-    #@item.update(item_params)
-    #if @item.valid?
-      #redirect_to item_path(item_params)
-    #else
-      #render 'edit'
-    #end
-  #end
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+  end
 
 
 
