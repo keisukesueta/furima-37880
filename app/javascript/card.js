d122e8b924cdaa4c5eb6349707bb1e1c31439ea1
keisukesueta <103096045+keisukesueta@users.noreply.github.com>
2022-05-18 21:35:07 +0900

    環境変数の編集

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_7e4c2066cd82640f57a267e3"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
