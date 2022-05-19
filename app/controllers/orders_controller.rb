ed0bbd3f612a78d7918033b7784aeb103b3592ea
keisukesueta <103096045+keisukesueta@users.noreply.github.com>
2022-05-19 14:44:48 +0900

    ログイン時のURLから購入ページへ遷移できない記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  # before_action :non_purchased_item, only: [:index, :create]
+  before_action :purchased_item, only: [:index, :create]
 
   def index
     @destination_order = DestinationOrder.new
@@ -36,8 +36,8 @@ class OrdersController < ApplicationController
     )
   end
 
-  # def non_purchased_item
-  # @item = Item.find(params[:item_id])
-  # redirect_to root_path if current_user.id == @item.user_id  #|| @item.order.present?
-  # end
+  def purchased_item
+  @item = Item.find(params[:item_id])
+  redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
 end
